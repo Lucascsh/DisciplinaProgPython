7eb844d182919833f9620550ea042233563aa9b5
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2022-08-04 21:49:12 -0300

    Arquivos da aula 1

---
+++ Codigos_Python/codigo01.py
@@ -0,0 +1,7 @@
+# print('Hello World.')
+
+# frase = input ('Digite algo: ')
+# print("Você digitou:", frase)
+
+idade = int(input ('Digite a sua idade: '))
+print ('voce ja dormiu:', idade/3, ' anos')
